46e52860fdbac729b795400c933d59570336b11a
rift <32559038+warp-records@users.noreply.github.com>
2025-12-24 22:17:58 -0500

    implement diagonal blocker test

--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,7 @@ mod tests {
 
     #[test]
     pub fn test_diagonals() {
-        let span = gen_diagonal(3, 0);
+        let span = gen_diagonal_ray(3, 0);
         let expected: u64 = chessboard!(
                 0b_00010000
                 0b_00101000
@@ -23,7 +23,7 @@ mod tests {
 
         assert_eq!(span, expected);
 
-        let span: u64 = gen_diagonal(1, 4);
+        let span: u64 = gen_diagonal_ray(1, 4);
         let expected: u64 = chessboard!(
             0b_00000100
             0b_00001000
@@ -41,7 +41,7 @@ mod tests {
     #[test]
     pub fn test_straight() {
 
-        let span: u64 = gen_straight(6, 3);
+        let span: u64 = gen_straight_ray(6, 3);
         let expected = chessboard!(
             0b_00000010
             0b_00000010
@@ -55,7 +55,7 @@ mod tests {
 
         assert_eq!(span, expected);
 
-        let span: u64 = gen_straight(2, 1);
+        let span: u64 = gen_straight_ray(2, 1);
         let expected = chessboard!(
             0b_00100000
             0b_11111111
@@ -71,10 +71,38 @@ mod tests {
     }
 
     #[test]
+    pub fn test_diagonal_blockers() {
+
+        let blockers = chessboard!(
+            0b_00000000
+            0b_00000000
+            0b_00010000
+            0b_00000010
+            0b_00000000
+            0b_00000010
+            0b_00010000
+            0b_00000000
+        );
+        let span: u64 = gen_clipped_diagonal(5, 4, blockers);
+        let expected: u64 = chessboard!(
+            0b_00000000
+            0b_00000000
+            0b_00010000
+            0b_00001010
+            0b_00000100
+            0b_00001010
+            0b_00010000
+            0b_00000000
+        );
+
+        assert_eq!(span, expected);
+    }
+
+    #[test]
     pub fn test_knight() {
 
         let span: u64 = gen_knight(6, 4);
-        let expected = chessboard!(
+        let expected: u64 = chessboard!(
             0b_00000000
             0b_00000000
             0b_00000101
@@ -88,3 +116,21 @@ mod tests {
         assert_eq!(span, expected);
     }
 }
+
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+
+// 0b_00000000
+// 0b_00000000
+// 0b_00000000
+// 0b_00000000
+// 0b_00000000
+// 0b_00000000
+// 0b_00000000
+// 0b_00000000
